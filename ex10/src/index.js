47a6ba210b452bb228794751f291aa7d1c68c1d6
desmosedici54 <mirrberr@gmail.com>
2022-04-04 18:11:34 +0200

    Adding the ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,8 @@
+function convertStrToInt(str) {
+    console.log(parseInt(str))
+}
+convertStrToInt("44");
+convertStrToInt("27");
+convertStrToInt("JeffBezos");
+
+module.exports = convertStrToInt;
